58ab337cfb0ed128d7c05633dfbfcbd474759abc
mag1cianag <mag1cian@aliyun.com>
2021-05-03 18:52:17 +0800

    lab2 1/3

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -5,18 +5,12 @@
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
 //! Transform an "absolute" 64-bit sequence number (zero-indexed) into a WrappingInt32
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
-WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
-}
+WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) { return isn + uint32_t(n); }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
 //! \param n The relative sequence number
@@ -29,6 +23,19 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t ans1;
+    uint64_t ans2;
+    if (n - isn < 0) {
+        ans1 = uint64_t(n - isn + (1LL << 32));
+    } else {
+        ans1 = uint64_t(n - isn);
+    }
+    if (ans1 >= checkpoint)
+        return ans1;
+    ans1 |= (checkpoint >> 32) << 32;
+    while (ans1 <= checkpoint) {
+        ans1 += (1LL << 32);
+    }
+    ans2 = ans1 - (1LL << 32);
+    return (checkpoint - ans2 < ans1 - checkpoint) ? ans2 : ans1;
 }
